57f468a321419cee56a04e8a68f5f92de1a02380
Ricardo Campos <ricardo@ricardocampos.blog>
2020-11-04 08:27:02 -0300

    - Updates and new script for windows

--- testIt.bat
+++ testIt.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
 
 SET "JAVA_HOME=C:\java-11-openjdk-11.0.8.10-1.windows.ojdkbuild.x86_64"
-mvn package -DskipTests=true -DfailIfNoTests=false
+
+:: Unit Tests
+mvn test -DfailIfNoTests=falsegit 
